aaf55c72c2433e1ab471ba9653c201bccc351373
Moody <mrahman01@stuy.edu>
2018-09-20 21:25:26 -0400

    final release ig

---
+++ CtoFTester.java
@@ -0,0 +1,17 @@
+public class CtoFTester{
+
+	public static double ctof(int inpC){
+		return inpC*(9.0/5.0)+32.0;}
+
+	public static double ftoc(int inpF){
+		return (inpF-32.0)*(5.0/9.0);}
+
+	public static void printNum(double inp){
+		System.out.println(inp);}
+
+
+	public static void main(String[] args){
+		printNum(ctof(0));
+		printNum(ftoc(212));
+    }
+}
