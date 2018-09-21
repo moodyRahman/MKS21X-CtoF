e1b818adeedf8f3d0edbd4e03b81d7bc5503a5a4
Moody <mrahman01@stuy.edu>
2018-09-20 20:59:57 -0400

    old version before main wrapper idea

---
+++ Ctof.java
@@ -0,0 +1,17 @@
+public class Ctof{
+
+
+	public static int ctof(inpC){
+		return inpC;}
+
+	public static int ftoc(inpF){
+		return inpF;}
+
+	public static String printer(stg){
+		System.out.println(stg);}
+
+
+	public static void main(String[] args){
+		printer(ftoc(44));}
+
+}
